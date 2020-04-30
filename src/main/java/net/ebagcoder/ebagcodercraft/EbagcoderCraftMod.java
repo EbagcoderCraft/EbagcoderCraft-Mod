7768f8c5c8ddb25fe696111b6342e351a4bd3445
EbagcoderCraft <ebagcodercraft.work@gmail.com>
2020-04-29 20:46:23 -0400

    Update EbagcoderCraftMod.java

--- src/main/java/net/ebagcoder/ebagcodercraft/EbagcoderCraftMod.java
+++ src/main/java/net/ebagcoder/ebagcodercraft/EbagcoderCraftMod.java
@@ -3,8 +3,11 @@ package net.ebagcoder.ebagcodercraft;
 import net.fabricmc.api.ModInitializer;
 
 public class EbagcoderCraftMod implements ModInitializer {
+	public static final Item FABRIC_ITEM = new Item(new Item.Settings().group(ItemGroup.MISC));
+
+	
 	@Override
 	public void onInitialize() {
-
-	}
+ Registry.register(Registry.ITEM, new Identifier("flight_ring", "fabric_item"), FABRIC_ITEM);
+    }
 }
